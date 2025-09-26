449471b049b12166e23136f41579dce57b94c74e
Stan1-0 <stankofb@gmail.com>
2025-09-26 21:30:48 +0000

    `Changed datetime.now() to datetime.date.today() in calculate_future_date function.`

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -9,12 +9,12 @@ def display_current_datetime():
     
 def calculate_future_date():
     number_of_days = int(input("Enter the number of days to add to the current date: "))
-    current_date_time = datetime.datetime.now()
+    current_date_time = datetime.date.today()
     
     future_date = current_date_time + timedelta(days=number_of_days)
     
     
-    print(f"Future date: {future_date.strftime("%Y-%m-%d")}")
+    print(f"Future date: {future_date}")
     
 calculate_future_date()
     
