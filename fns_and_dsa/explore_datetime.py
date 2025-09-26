38b3e6f64192a4f1bebd357786e2a0bb81105165
Stan1-0 <stankofb@gmail.com>
2025-09-26 21:32:33 +0000

    `Updated calculate_future_date function to use datetime.now() instead of date.today() and added date formatting to print output.`

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -9,12 +9,12 @@ def display_current_datetime():
     
 def calculate_future_date():
     number_of_days = int(input("Enter the number of days to add to the current date: "))
-    current_date_time = datetime.date.today()
+    current_date_time = datetime.datetime.now()
     
     future_date = current_date_time + timedelta(days=number_of_days)
     
     
-    print(f"Future date: {future_date}")
+    print(f"Future date: {future_date.strftime("%Y-%m-%d")}")
     
 calculate_future_date()
     
