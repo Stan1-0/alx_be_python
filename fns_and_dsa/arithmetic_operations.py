006dc2dd527ad1538e025cd7688610ad3b484d2d
Stan1-0 <stankofb@gmail.com>
2025-09-26 20:22:20 +0000

    `Refactored perform_operation function to return values instead of printing them.`

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,20 +1,20 @@
 def perform_operation(num1, num2, operation):
     
     if operation == "add":
-        result = num1 + num2
+        return num1 + num2
     elif operation == "subtract":
-        result = num1 - num2
+        return num1 - num2
     elif operation == "multiply":
-        result = num1 * num2
+        return num1 * num2
     elif operation == "divide":
         if num2 == 0:
-            print(f"Error {ZeroDivisionError}")
+            return ZeroDivisionError
         else:
-            result = num1/ num2
+            return num1/ num2
     else:
         print("Invalid operation")
     
-    print(result)
+    
     
 
     
