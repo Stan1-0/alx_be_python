1cbc45d5b81b3a8b6bde71ec7fc172d10ef91ba3
Stan1-0 <stankofb@gmail.com>
2025-09-26 20:02:28 +0000

    created an arithemetic operation function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,22 @@
+def perform_operation(num1, num2, operation):
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+    
+    match operation:
+        case "add":
+            result = num1 + num2
+        case "subtract":
+            result = num1 - num2
+        case "multiply":
+            result = num1 * num2
+        case "divide":
+            if num2 > 0:
+                result = num1 / num2
+            else:
+                result = f"You cannot divide by 0. {ZeroDivisionError}"
+        case "_":
+            print("No operation selected")
+            
+    print(f"Result: {result}")
+            
