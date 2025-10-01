7a303ab24d024470d35bf9eaecf3cbe481c57233
Stan1-0 <stankofb@gmail.com>
2025-10-01 21:58:11 +0000

    Created my BankAccount OOP

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,23 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        if account_balance < 0:
+            raise ValueError("Account balance cannot be negative")
+        self.account_balance = account_balance
+        
+    def deposit(self, amount):
+        if amount > 0:
+            self.account_balance += amount
+            return True
+        else:
+
+            return False
+    
+    def withdraw(self, amount):
+        if amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        else:
+            return False        
+    
+    def display_balance(self):
+        return self.account_balance
